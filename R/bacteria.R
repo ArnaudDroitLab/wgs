c1fa8e548c10db8c012e204868d4674281b708cf
Charles Joly Beauparlant <Charles.Joly-Beauparlant@crchul.ulaval.ca>
2019-05-28 10:19:42 -0400

    Changed bacteria.rda to contain all bacteria metadata
    Signed-off-by: Charles Joly Beauparlant <Charles.Joly-Beauparlant@crchul.ulaval.ca>

---
+++ R/bacteria.R
@@ -0,0 +1,43 @@
+#' Genbank's bacterias metadata
+#'
+#' Fetch the metadata for all the bacterias in Genbank
+#'
+#' Data is obtained from the \code{assembly_summary.txt} file:
+#' ftp://ftp.ncbi.nlm.nih.gov/genomes/genbank/bacteria/assembly_summary.txt
+#'
+#' @return A \code{tibble} with the following columns:
+#' \itemize{
+#'   \item assembly_accession
+#'   \item refseq_category
+#'   \item organism_name
+#'   \item infraspecific_name
+#'   \item assembly_level
+#'   \item version_status
+#'   \item ftp_path
+#' }
+#'
+#' @examples
+#' \dontrun{
+#'   bacteria <- fetch_bacteria_metadata()
+#' }
+#'
+#' @importFrom magrittr %>%
+#' @importFrom readr read_tsv
+#' @importFrom dplyr select
+#'
+#' @export
+fetch_bacteria_metadata <- function() {
+  col_types <- rep("c", 22) %>% paste(collapse = "")
+
+  base_url <- "ftp://ftp.ncbi.nlm.nih.gov/genomes/genbank/bacteria"
+  url <- paste0(base_url, "/assembly_summary.txt")
+
+  read_tsv(url, skip = 1, col_types = col_types, quote = "") %>%
+      dplyr::select(assembly_accession = `# assembly_accession`,
+                    refseq_category,
+                    organism_name,
+                    infraspecific_name,
+                    assembly_level,
+                    version_status,
+                    ftp_path)
+}
--- data-raw/bacteria.R
+++ data-raw/bacteria.R
@@ -1,14 +1,4 @@
-require(tidyverse)
-require(rvest)
-require(usethis)
+require(wgs)
 
-base_url <- "https://ftp.ncbi.nlm.nih.gov/genomes/genbank/bacteria"
-bacteria_html <- read_html(base_url)
-
-bacteria <- bacteria_html %>%
-    html_nodes("a") %>%
-    html_text %>%
-    str_replace("\\/", "")
-
-bacteria[!str_detect(bacteria, "\\.txt$")][-1] %>%
-    usethis::use_data
+bacteria <- fetch_bacteria_metadata()
+usethis::use_data(bacteria)
